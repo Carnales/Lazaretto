743060e1fccb48ae0eaacbe197b41c826aec9dfc
stashito <s.ostyks@gmail.com>
2021-08-14 17:38:43 +0300

    comment lazeratto works

--- lazarette.js
+++ lazarette.js
@@ -1,3 +1,5 @@
+var replaced = false;
+
 function deleteElement() {
     console.log("Deleted");
 
@@ -8,7 +10,10 @@ function deleteElement() {
 
     //This function calls the replace button once comment fields is clicked on (twice :/)
     commentField.addEventListener("click", function() {
-        replaceSubmit();
+        if (!replaced) {
+          replaceSubmit();
+          replaced = true;
+        }
     });
 
     //data.parentNode.removeChild(data);
@@ -22,12 +27,29 @@ async function replaceSubmit() {
     await new Promise(r => setTimeout(r, 100));
     //replaces the Button NEEDS CSS OR SMTH CAUSE IT'S DOGWATER
     $("#submit-button").after("<tp-yt-paper-button id='replaceButton' style='background: green; font-size: 15px; color: white'>Lazaretto!</tp-yt-paper-button>").appendTo("body");
-
+    console.log("DOING THIS SHIT");
     replaceButton = document.getElementById("replaceButton");
     replaceButton.addEventListener("click", function(event) {
         commentAnalysis();
         replaceButton.innerHTML = "AI is Processing...";
         event.preventDefault();
+    });
+
+    // inject the warning modal
+    $.get(chrome.runtime.getURL('/modal.html'), function(data) {
+      $($.parseHTML(data)).appendTo('body');
+
+      // replace the warning message
+
+      // move the OG comment button here
+      $("#place-for-button").append($("#submit-button"));
+      document.getElementById("submit-button").addEventListener("click", commented);
+
+      // listen to go back
+      var modal = document.getElementById("myModal");
+
+      document.getElementById("reviewButton").addEventListener("click", goBack);
+
     });
 }
 
@@ -45,32 +67,36 @@ async function commentAnalysis() {
     var message = "";
 
     // assign a warning message
-    if (lvl < 50)
+    var rev_btn = document.getElementById("reviewButton");
+    var sub_btn = document.getElementById("submit-button");
+    if (lvl < 50) {
       message = "Comment looks good! Go ahead and post it.";
-    else
+      rev_btn.style.display = "none";
+      sub_btn.style.display = "block";
+    }
+    else {
       message = "Woah! This comment might be offensive, so be sure to edit it before you post.";
+      rev_btn.style.display = "block";
+      sub_btn.style.display = "none";
+    }
 
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    document.getElementById("warning-message").innerHTML = message;
 
-    // inject the warning modal
-    $.get(chrome.runtime.getURL('/modal.html'), function(data) {
-      $($.parseHTML(data)).appendTo('body');
-
-      // replace the warning message
-      document.getElementById("warning-message").innerHTML = message;
-
-      // move the OG comment button here
-       $("#place-for-button").append($("#submit-button"));
-
-       // listen to go back
-       var modal = document.getElementById("myModal");
-
-       document.getElementById("reviewButton").addEventListener("click", goBack);
-
-    });
 }
+
+function commented() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+
+  replaceButton = document.getElementById("replaceButton");
+  replaceButton.innerHTML = "Another comment?";
+}
+
 function goBack() {
     var modal = document.getElementById("myModal");
-    modal.remove();
+    modal.style.display = "none";
 
     replaceButton = document.getElementById("replaceButton");
     replaceButton.innerHTML = "Be nicer this time!";
--- toxic.js
+++ toxic.js
@@ -2,5 +2,6 @@ async function getToxicityLevel(text) {
   // implement ML over here
   await new Promise(r => setTimeout(r, 2000));
   // return percentage of toxicity out of 100
-  return 90;
+
+  return text.includes("bad") ? 100 : 0;
 }
